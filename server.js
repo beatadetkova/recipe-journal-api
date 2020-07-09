ef1f099c322f28d1b30e930e4aac9f3c240136f8
Beata Detkova <beata.detkova.jr@gmail.com>
2020-07-09 18:07:01 +0200

    Added "Add recipe" endpoint

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const meals = require('./meals');
-const recipes = require('./recipes');
+const meals = require('./meals.json');
+const fs = require('fs');
 
 app.use(cors());
+app.use(express.json());
 
 //setting the port.
 app.set('port', process.env.PORT || 5000);
@@ -13,9 +14,20 @@ app.get('/meals',(req, res) => {
  res.json(meals);
 });
 
-app.get(`/meals/:id`, (req, res) => {
+app.get(`/meals/:id/recipes`, (req, res) => {
   const id = req.params.id;
+  const recipes = require('./recipes.json');
   res.json(recipes[id]);
+});
+
+app.post('/meals/:id/recipes/add', function (req, res) {
+  const mealId = req.params.id;
+  var recipes = require('./recipes.json');
+  var body = req.body;
+  body.id = Date.now();
+  recipes[mealId] = recipes[mealId].concat([body]);
+  fs.writeFileSync('recipes.json', JSON.stringify(recipes, null, 4));
+  res.json(recipes[mealId])
 });
 
 //Binding to localhost://5000
