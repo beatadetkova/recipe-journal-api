45b2df1728c6f6e46bf3a958bf309a12d0ee79ec
Beata Detkova <beata.detkova.jr@gmail.com>
2020-07-17 18:34:45 +0200

    Extracted get-all-meals logic

--- server.js
+++ server.js
@@ -2,31 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
+const port = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const getAllMeals = require('./helpers/get-all-meals.js')
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'recipe-journal-db';
+const dbOpts = {
+  url: 'mongodb://localhost:27017',
+  dbName: 'recipe-journal-db'
+}
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(dbOpts.url, { useUnifiedTopology: true });
 let meals;
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  const collection = db.collection('meals');
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    meals = docs;
-    client.close();
-  });
-});
 
 app.use(cors());
 app.use(express.json());
@@ -59,6 +46,7 @@ app.post('/meals/:id/recipes/add', function (req, res) {
 });
 
 //Binding to localhost://5000
-app.listen(5000,() => {
- console.log('Express server started at port 5000');
+app.listen(port, async () => {
+  meals = await getAllMeals(client, dbOpts.dbName)
+  console.log(`Express server started at port ${port}`);
 });
