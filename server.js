09d2031191aa385f76d22727b7625bdb77b657e6
Beata Detkova <beata.detkova.jr@gmail.com>
2020-07-22 19:32:53 +0200

    Updated endpoint + fixed client close issue

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const fs = require('fs');
 const port = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
-const getAllMeals = require('./helpers/get-all-meals.js')
+const getAllMeals = require('./helpers/get-all-meals.js');
+const getRecipes = require('./helpers/get-recipes.js');
 
 const dbOpts = {
   url: 'mongodb://localhost:27017',
@@ -22,10 +23,9 @@ app.get('/meals',(req, res) => {
  res.json(meals);
 });
 
-app.get(`/meals/:id/recipes`, (req, res) => {
-  const id = req.params.id;
-  const recipes = require('./recipes.json');
-  res.json(recipes[id]);
+app.get(`/meals/:mealId/recipes`, async (req, res) => {
+  const recipes = await getRecipes(client, dbOpts.dbName, req.params.mealId);
+  res.json(recipes);
 });
 
 app.post('/meals/:id/recipes/add', function (req, res) {
@@ -44,6 +44,11 @@ app.post('/meals/:id/recipes/add', function (req, res) {
 
 //Binding to localhost://5000
 app.listen(port, async () => {
+  await client.connect()
   meals = await getAllMeals(client, dbOpts.dbName)
   console.log(`Express server started at port ${port}`);
 });
+
+process.on('beforeExit', async () => {
+  await client.close()
+})
