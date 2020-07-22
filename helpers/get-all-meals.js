4c9f61ff98c26e1aeea42960173fbc5bc41f705f
Beata Detkova <beata.detkova.jr@gmail.com>
2020-07-22 19:33:07 +0200

    Fixed client close issue

--- helpers/get-all-meals.js
+++ helpers/get-all-meals.js
@@ -1,10 +1,5 @@
 module.exports = async (client, dbName) => {
-    const meals = await client.connect()
-        .then(() => {
-            const db = client.db(dbName);
-            const collection = db.collection('meals');
-            return collection.find({}).toArray()
-        })
-    client.close()
-    return meals
+    const db = client.db(dbName);
+    const collection = db.collection('meals');
+    return collection.find({}).toArray()
 }
